ea9c10b84bc13f0142f3fe5b9251d0edcef31743
Scott Falbo <scottfalboart@gmail.com>
2021-04-21 08:25:57 -0700

    writing unit test against db

--- PortfolioTests/DataBaseTests.cs
+++ PortfolioTests/DataBaseTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Portfolio.Models;
 using Portfolio.Models.Interface;
 
 namespace PortfolioTests
@@ -8,9 +9,24 @@ namespace PortfolioTests
     {
 
         [TestMethod]
-        public void TestMethod1()
+        public async void TestMethod1()
         {
-            
+            Project testProject = new Project
+            {
+                Id = 6,
+                Title = "Test Project",
+                SourceURL = "whatever",
+                Description = "It's a thing",
+                Order = 1
+            };
+            await CreateProject(testProject);
+
+            Project testGet = await GetProject(6);
+
+            string actual = testGet.Title;
+            string expected = testProject.Title;
+
+            Assert.AreEqual(actual, expected);
         }
     }
 }
