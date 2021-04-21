d3e41281e75843cbe37a5870c3c32eaa5bd0bad0
Scott Falbo <scottfalboart@gmail.com>
2021-04-20 20:49:44 -0700

    added unitx testing

---
+++ PortfolioTests/DataBaseTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioTests
+{
+    [TestClass]
+    public class DataBaseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
---
+++ PortfolioTests/Mock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortfolioTests
+{
+    public abstract class Mock : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
