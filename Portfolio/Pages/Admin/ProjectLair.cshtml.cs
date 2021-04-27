ecdf701bae901b8a354c7d022b7099abb3260aab
Scott Falbo <scottfalboart@gmail.com>
2021-04-27 13:47:53 -0700

    scaffolded out the admin section

---
+++ Portfolio/Pages/Admin/ProjectLair.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+using Portfolio.Models.Interface;
+
+namespace Portfolio.Pages.Admin
+{
+    public class ProjectLairModel : PageModel
+    {
+        public IAdmin _adminContext;
+        public IArtAdmin _artAdminContext;
+
+        public ProjectLairModel(IAdmin context, IArtAdmin artAdmin)
+        {
+            _adminContext = context;
+            _artAdminContext = artAdmin;
+        }
+
+        public List<Project> ProjectList { get; set; }
+
+
+        public async Task OnGet()
+        {
+            try
+            {
+                ProjectList = await _adminContext.GetProjects();
+            }
+            catch (Exception e)
+            {
+                RedirectToPage("Opps", new { error = e });
+            }
+        }
+    }
+}
