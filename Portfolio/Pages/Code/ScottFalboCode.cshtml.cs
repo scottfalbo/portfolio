73480acdadf183bfbcb84cc3528041e9cb7302c7
Scott Falbo <scottfalboart@gmail.com>
2021-05-03 16:14:55 -0700

    basic layout scaffolded

---
+++ Portfolio/Pages/Code/ScottFalboCode.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+using Portfolio.Models.Interface;
+
+namespace Portfolio.Pages.Code
+{
+    public class ScottFalboCodeModel : PageModel
+    {
+        public IAdmin _adminContext;
+
+        public ScottFalboCodeModel(IAdmin context)
+        {
+            _adminContext = context;
+        }
+
+        public List<Project> ProjectList { get; set; }
+
+        public async Task OnGet()
+        {
+            try
+            {
+                ProjectList = await _adminContext.GetProjects();
+            }
+            catch (Exception e)
+            {
+                RedirectToPage("Opps", new { error = e });
+            }
+
+        }
+    }
+}
