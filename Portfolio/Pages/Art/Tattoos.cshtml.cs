71c0b02d96f11bfc305d29e0deca7f210bd23c10
Scott Falbo <scottfalboart@gmail.com>
2021-05-11 21:16:41 -0700

    made tattoo portfolio partial, set up TattoosModel

--- Portfolio/Pages/Art/Tattoos.cshtml.cs
+++ Portfolio/Pages/Art/Tattoos.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+using Portfolio.Models.Interface;
 
 namespace Portfolio.Pages.Art
 {
     public class TattoosModel : PageModel
     {
-        public void OnGet()
+        public IArtAdmin _adminContext;
+
+        public TattoosModel(IArtAdmin admin)
         {
+            _adminContext = admin;
+        }
+
+        [BindProperty]
+        public List<Tattoo> Tattoos { get; set; }
+
+        public async Task OnGet()
+        {
+            Tattoos = await _adminContext.GetTattoos();
         }
     }
 }
