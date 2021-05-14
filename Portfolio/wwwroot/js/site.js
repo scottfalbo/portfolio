60ea85d22b7f95d313b855f9e4c586fc12f56953
Scott Falbo <scottfalboart@gmail.com>
2021-05-13 21:29:21 -0700

    added jquery to toggle gallery viewer on and off with thumbs and close button

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿'use strict';
 
+$(function () {
+    $('.gallery-toggle').click(function () {
+        $('#tattoo-gallery').toggleClass('show-gallery');
+    });
+});
+
+$(function () {
+    $('.close-gallery').click(function () {
+        $('#tattoo-gallery').toggleClass('show-gallery');
+    });
+});
 
 
