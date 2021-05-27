11d9796283ec6356cbc896d5e72d52ec2746c7e5
Scott Falbo <scottfalboart@gmail.com>
2021-05-27 13:57:49 -0700

    scaffolded forms and made request form object from input

--- Portfolio/Pages/Art/Booking.cshtml.cs
+++ Portfolio/Pages/Art/Booking.cshtml.cs
@@ -4,13 +4,51 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Models;
+using Portfolio.Models.Interface;
 
 namespace Portfolio.Pages.Art
 {
     public class BookingModel : PageModel
     {
-        public void OnGet()
+        public IAdmin _admin;
+
+        public BookingModel(IAdmin admin)
         {
+            _admin = admin;
+        }
+
+        public HomePage HomePage { get; set; }
+
+        [BindProperty]
+        public RequestForm RequestForm { get; set; }
+
+        public async Task OnGet()
+        {
+            HomePage = await _admin.GetHomePage("Booking");
+            RequestForm = new RequestForm();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> OnPostSend()
+        {
+            
+            return Redirect("/Art/Booking");
         }
     }
+
+    /// <summary>
+    /// Email request object
+    /// </summary>
+    public class RequestForm 
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Idea { get; set; }
+        public string Availability { get; set; }        
+    }
+
 }
