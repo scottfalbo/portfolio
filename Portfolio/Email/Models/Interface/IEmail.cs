56f5f648d068542bd158e77b5fd14066a5a4a492
Scott Falbo <scottfalboart@gmail.com>
2021-05-27 14:19:17 -0700

    built out sendgrid models, interface and services, setup transient

---
+++ Portfolio/Email/Models/Interface/IEmail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Email.Models.Interface
+{
+    interface IEmail
+    {
+        public Task<EmailResponse> SendEmailAsync(Message inboundData);
+    }
+}
