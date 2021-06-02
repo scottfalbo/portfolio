d8fe6d24ce7c9484395f5aa2a4c467aecafa54e9
Scott Falbo <scottfalboart@gmail.com>
2021-06-02 14:54:01 -0700

    changed contact partial for contact view component

---
+++ Portfolio/Components/Contact.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Email.Models.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Components
+{
+    [ViewComponent]
+    public class ContactViewComponent : ViewComponent
+    {
+        private readonly IEmail _email;
+
+        public ContactViewComponent(IEmail email)
+        {
+            _email = email;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewModel vm = new ViewModel()
+            {
+                Hello = "hello world"
+            };
+            return View(vm);
+        }
+
+        public class ViewModel
+        {
+            public string Hello { get; set; }
+        }
+    }
+
+
+
+}
