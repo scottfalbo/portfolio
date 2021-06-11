f6060bb2bbbc75593d8ab2fba94365b9c2f93a82
Scott Falbo <scottfalboart@gmail.com>
2021-06-10 22:16:15 -0700

    added vault nuget packages

--- Portfolio/Data/PortfolioDbContext.cs
+++ Portfolio/Data/PortfolioDbContext.cs
@@ -44,8 +44,8 @@ namespace Portfolio.Data
                 NormalizedName = "admin"
             });
 
-            var admin = Configuration["Portfolio:AdminName"];
-            var adminPass = Configuration["Portfolio:AdminPass"];
+            var admin = Configuration["Portfolio:Name"];
+            var adminPass = Configuration["Portfolio:Pass"];
 
             var hasher = new PasswordHasher<ApplicationUser>();
             modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
