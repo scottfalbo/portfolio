a601fdea1402a46ef2a169871c5fea162a0757a2
Scott Falbo <scottfalboart@gmail.com>
2021-08-21 00:53:54 -0700

    undid all of the react native stuff, going to use jquery

--- Portfolio/Startup.cs
+++ Portfolio/Startup.cs
@@ -25,6 +25,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace Portfolio
 {
     public class Startup
@@ -39,6 +40,7 @@ namespace Portfolio
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddRazorPages(options =>
             {
                 //options go here
@@ -98,6 +100,7 @@ namespace Portfolio
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
