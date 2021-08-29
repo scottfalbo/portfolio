8b5378a520c8872c2f39bbae97e2f66645e6a1d5
Scott Falbo <scottfalboart@gmail.com>
2021-08-28 22:45:54 -0700

    Added a popup to verify when user navigate away from page to studio arcanum.

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -172,7 +172,6 @@ $(document).ready(function () {
         $(this).children('.mouse-over-tooltip').addClass('hide-me');
     });
 });
-
 $(document).on('mouseover', function (e) {
     $('.mouse-over-tooltip').css({
         left: e.pageX,
@@ -180,129 +179,19 @@ $(document).on('mouseover', function (e) {
     });
 });
 
+// Technology dropdown menu controls
 $(document).ready(function () {
-    console.log('what what');
     $('.technology-dropdown').slideToggle();
 });
-
 $(function () {
     $('.technologies-menu-button').click(function () {
         $('.technology-dropdown').slideToggle();
     });
 });
 
-//pagination stuff
-// function getCarouselIndex() {
-//     const index = ($('figure.active').index()) + 2;
-//     $('#limit').val(index);
-//     console.log(index);
-// }
-
-
-
-
-//------------------------ Google Map API stuff
-
-// //https://developers.google.com/maps/documentation/javascript/examples/style-array
-// //Google maps API callback function
-// function initMap() {
-
-//     var myAddress = new google.maps.LatLng(47.660103768145014, -122.35032563073302);
-
-//     var mapOptions = {
-//         center: myAddress,
-//         zoom: 10,
-//         minZoom: 10,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP,
-//         //https://developers.google.com/maps/documentation/javascript/examples/style-array
-//         styles: [
-//             { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-//             { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-//             { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-//             {
-//                 featureType: "administrative.locality",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#d59563" }],
-//             },
-//             {
-//                 featureType: "poi",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#d59563" }],
-//             },
-//             {
-//                 featureType: "poi.park",
-//                 elementType: "geometry",
-//                 stylers: [{ color: "#263c3f" }],
-//             },
-//             {
-//                 featureType: "poi.park",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#6b9a76" }],
-//             },
-//             {
-//                 featureType: "road",
-//                 elementType: "geometry",
-//                 stylers: [{ color: "#38414e" }],
-//             },
-//             {
-//                 featureType: "road",
-//                 elementType: "geometry.stroke",
-//                 stylers: [{ color: "#212a37" }],
-//             },
-//             {
-//                 featureType: "road",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#9ca5b3" }],
-//             },
-//             {
-//                 featureType: "road.highway",
-//                 elementType: "geometry",
-//                 stylers: [{ color: "#746855" }],
-//             },
-//             {
-//                 featureType: "road.highway",
-//                 elementType: "geometry.stroke",
-//                 stylers: [{ color: "#1f2835" }],
-//             },
-//             {
-//                 featureType: "road.highway",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#f3d19c" }],
-//             },
-//             {
-//                 featureType: "transit",
-//                 elementType: "geometry",
-//                 stylers: [{ color: "#2f3948" }],
-//             },
-//             {
-//                 featureType: "transit.station",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#d59563" }],
-//             },
-//             {
-//                 featureType: "water",
-//                 elementType: "geometry",
-//                 stylers: [{ color: "#17263c" }],
-//             },
-//             {
-//                 featureType: "water",
-//                 elementType: "labels.text.fill",
-//                 stylers: [{ color: "#515c6d" }],
-//             },
-//             {
-//                 featureType: "water",
-//                 elementType: "labels.text.stroke",
-//                 stylers: [{ color: "#17263c" }],
-//             },
-//         ],
-//     };
-
-//     var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
-
-//     var marker = new google.maps.Marker({
-//         position: myAddress
-//     });
-
-//     marker.setMap(map); 
-// }
-
+// Leave page popup for studio arcanum links
+$(function () {
+    $('.leave-page-link').click(function() {
+        $('.leave-page').removeClass('hide-me');
+    });
+});
